12c29aaa22faefad4fd120e81c52e011630b1e8f
Sung Jun Jin <sungjun@peoplefund.co.kr>
2022-03-03 22:56:33 +0900

    feat: setState 실습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import propTypes from "prop-types";
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      </div> 
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  add = () => {
+    console.log("add");
+    this.setState(current => ({count: current.count + 1}));
+  }
+
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  }
+
+
+  render() {
+    return (
+      <div> 
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
